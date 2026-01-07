566d0e65d83464b3c39360de615dce6d912ee849
rewine <luhongxu@deepin.org>
2026-01-07 11:41:48 +0800

    Disable autoformat for json

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable autoformat for JSON buffers (LazyVim checks b.autoformat)
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "json", "jsonc" },
+	callback = function()
+		vim.b.autoformat = false
+	end,
+})
